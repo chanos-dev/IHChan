d755db4ed4939aed18d0ac02bec3d564f38eafb1
chanos-dev <chanos.dev@gmail.com>
2021-05-11 01:52:03 +0900

    poc : Geomap 지도 색상 변경

--- IHChan/Poc/Form1.cs
+++ IHChan/Poc/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media;
 
 namespace Poc
 {
@@ -29,19 +30,21 @@ namespace Poc
             var values = new Dictionary<string, double>();
 
             values["MX"] = 1;
-            values["CA"] = 10;
-            values["US"] = 100;
-            values["IN"] = r.Next(0, 100);
+            values["CA"] = 1290000;
+            values["US"] = 33479348;
+            values["IN"] = 22940516;
             values["CN"] = r.Next(0, 100);
             values["JP"] = r.Next(0, 100);
-            values["BR"] = r.Next(0, 100);
+            values["BR"] = 15184790;
             values["DE"] = r.Next(0, 100);
             values["FR"] = r.Next(0, 100);
             values["GB"] = r.Next(0, 100);
-            values["KR"] = 10;
+            values["KR"] = 127772;
+            values["RU"] = 4888727;
 
-            geoMap1.LandClick += GeoMap1_LandClick;            
-            geoMap1.BackColor = Color.Gray;  
+            geoMap1.LandClick += GeoMap1_LandClick;
+
+            //geoMap1.BackColor = System.Drawing.Color.Purple;
 
             geoMap1.HeatMap = values; 
             geoMap1.Source = "World/World.xml";            
@@ -50,13 +53,9 @@ namespace Poc
             geoMap1.EnableZoomingAndPanning = true;
 
             // show a value of heatmap on the Geomap.
-            geoMap1.Hoverable = true;
+            geoMap1.Hoverable = true; 
 
-            var brush = new System.Windows.Media.SolidColorBrush();
-
-            brush.Color = System.Windows.Media.Color.FromRgb(62, 62, 66);
-
-            geoMap1.DefaultLandFill = brush;            
+            geoMap1.DefaultLandFill = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(255, 242, 242));
 
             this.Controls.Add(geoMap1);            
             geoMap1.Dock = DockStyle.Fill;
@@ -67,14 +66,21 @@ namespace Poc
 
             //geoMap1.LandStroke = brush;
 
-            geoMap1.Base.LandStroke = brush;
+            geoMap1.Base.LandStroke = new System.Windows.Media.SolidColorBrush(Colors.Gray);                       
 
-            
+            geoMap1.GradientStopCollection = new GradientStopCollection()
+            { 
+                new GradientStop(System.Windows.Media.Color.FromRgb(250,233,233), 0.00),
+                new GradientStop(System.Windows.Media.Color.FromRgb(234,207,207), 0.25),
+                new GradientStop(System.Windows.Media.Color.FromRgb(191,132,132), 0.55), 
+                new GradientStop(System.Windows.Media.Color.FromRgb(158,76,76), 0.75),
+                new GradientStop(System.Windows.Media.Color.FromRgb(113,0,0), 1.00),
+            };
 
             //var lang = new Dictionary<string, string>();
             //lang["KR"] = "Korea";
-            //geoMap1.LanguagePack = lang;            
-        }
+            //geoMap1.LanguagePack = lang; 
+        } 
 
         private void Base_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
